7eadf94c1df89f0d2304dcf7c3ebd59b6544e016
Thirupravi <63950737+Thirupravi@users.noreply.github.com>
2021-03-01 21:54:39 +0530

    Worked on UI

---
+++ mail/src/Components/Messagecontent/Messagecontent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Messagecontent.css";
+
+export default function Messagecontent(Message) {
+  return (
+    <div className="message">
+      <p>"Select item to read "</p>
+    </div>
+  );
+}
